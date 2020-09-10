7f34849ea3e3a9030529b83d550bfe71fa4583ec
Thomas Michelet <thomas.michelet@wifirst.fr>
2020-09-10 07:48:40 +0200

    Remove unused variables

--- src/components/text-input/text-input-view.js
+++ src/components/text-input/text-input-view.js
@@ -1,19 +1,17 @@
-import React, { Fragment } from 'react'
+import React from 'react'
 import { TextField } from "@material-ui/core"
 import { Field } from 'react-final-form'
 
 import { InputError, isInputWithError } from '../../utils'
 
-const TextInput = ({name, input, meta}) => {
+const TextInput = ({name}) => {
   return (
     <Field name={name}>
       {({input, meta}) => {
         return (
-          <Fragment>
-            <TextField label={input.name} name={input.name} value={input.value}
-                       onChange={input.onChange} placeholder={input.placeholder}
-                       error={isInputWithError(meta)} helperText={InputError(meta)} />
-          </Fragment>
+          <TextField label={input.name} name={input.name} value={input.value}
+                     onChange={input.onChange} placeholder={input.placeholder}
+                     error={isInputWithError(meta)} helperText={InputError(meta)} />
         )
       }}
     </Field>
